fe195407575985ff493a11231df0197acf3a68ab
Egor Malyutin <egorcod.tk@gmail.com>
2019-10-10 18:31:17 +0600

    Upd

--- dwm/bar.sh
+++ dwm/bar.sh
@@ -4,7 +4,7 @@ while true; do
 	status=""
 
 	# Battery
-	status+=`acpi | grep -oP ":\s*\K.*%" | sed -s 's/U.*n,/CHR/; s/Full,/FULL/; s/D.*g,/DCHR/'`
+	status+=`acpi | grep -oP ":\s*\K.*%" | sed -s 's/U.*n,/CHR/; s/C.*g,/CHR/; s/Full,/FULL/; s/D.*g,/DSCHR/'`
 	
 	# Volume
 	vol=`amixer get Master`
@@ -13,6 +13,10 @@ while true; do
 	muted=`echo "$vol" | grep -oP '\[(off|on)\]' | sed -s 's/\[//; s/\]//' | head -1`
 
 	status+="  │  ${muted^^} $perc"
+
+	# Date
+	date=`LANG=en date +'%H:%M %d/%m/%y (%a)'`
+	status+="  │  $date "
 
 	xsetroot -name "$status"
 	# echo "$status"
