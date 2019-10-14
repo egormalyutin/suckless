bf11f1682c23718e4005658e742b8666ae3869d1
Egor Malyutin <egorcod.tk@gmail.com>
2019-10-14 23:13:36 +0600

    Upd

--- dwm/config.h
+++ dwm/config.h
@@ -32,7 +32,8 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	// { "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	// { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ NULL, NULL, NULL, 0, 0, 0 }
 };
 
 /* layout(s) */
